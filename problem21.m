81e447f2bade0314cd48753db64a5f3b59fb713b
liuzheng712 <liuzheng712@gmail.com>
2012-10-28 18:22:52 +0800

    finish problem 21

--- problem21.m
+++ problem21.m
@@ -1,12 +1,22 @@
 function [sum,a]=problem21(num)
-% answer 502
+% Let d(n) be defined as the sum of proper divisors of n (numbers less than
+% n which divide evenly into n).
+% If d(a) = b and d(b) = a, where a  b, then a and b are an amicable pair 
+% and each of a and b are called amicable numbers.
+% 
+% For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22,
+% 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 
+% 1, 2, 4, 71 and 142; so d(284) = 220.
+% 
+% Evaluate the sum of all the amicable numbers under 10000.
+
 for i=2:num
     a(i)=amicable(i);
 end
 sum=0;
 
 for i=2:num
-    if a(i)<=num && a(i)==a(a(i)) && a(i)~=i
+    if a(i)<=num && i==a(a(i)) && a(i)~=i
         sum=sum+a(i);
     end
 end
